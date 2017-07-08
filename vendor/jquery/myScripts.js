e2f7bf8c032404bdca420e9454049b427594c941
Carlos <carlosll_24@hotmail.com>
2017-07-08 13:10:26 -0500

    banner

---
+++ vendor/jquery/myScripts.js
@@ -0,0 +1,14 @@
+var a =['assets/images/home/slide1.jpg','assets/images/home/slide2.jpg','assets/images/home/slide3.jpg']
+var i=0
+function show(){
+  $("#banner").attr("src",a[i])
+  i++
+  if (i==3) i=0;
+}
+setInterval(show,2000);
+$(document).ready(function(){
+  $("#banner-botones a").click(function(){
+    $("#banner").attr("src",a[$(this).text()])
+    i = $(this).text()
+  })
+})
