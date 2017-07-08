2b20deeab3330ff7dba3b48781b5e24a391589cd
Carlos <carlosll_24@hotmail.com>
2017-07-08 15:32:35 -0500

    paginas effect

--- vendor/jquery/myScripts.js
+++ vendor/jquery/myScripts.js
@@ -1,12 +1,12 @@
-var a =['assets/images/home/slide1.jpg','assets/images/home/slide2.jpg','assets/images/home/slide3.jpg']
-var i=0
-function show(){
-  $("#banner").attr("src",a[i])
-  i++
-  if (i==3) i=0;
-}
-setInterval(show,2000);
 $(document).ready(function(){
+  var a =['assets/images/home/slide1.jpg','assets/images/home/slide2.jpg','assets/images/home/slide3.jpg']
+  var i=0
+  function show(){
+    $("#banner").attr("src",a[i])
+    i++
+    if (i==3) i=0;
+  }
+  setInterval(show,2000);
   $("#banner-botones a").click(function(){
     $("#banner").attr("src",a[$(this).text()])
     i = $(this).text()
@@ -14,5 +14,52 @@ $(document).ready(function(){
   $("#banner-text").animate({
     top: "0"
   },2000);
-  $()
+  $("#bloq1").animate({
+    left: "0"
+  },2000);
+  $("#bloq2").animate({
+    right: "0"
+  },2000);
+  $("#footer").animate({
+    opacity: "1"
+  },3000)
+  $("#banner-txt").animate({
+    top:"0"
+  },2000);
+
+  $("#luchita").animate({
+    left: "0"
+  },1000);
+  $("#jaimito").animate({
+    right: "0"
+  },1000);
+
+  $(" #luchito").animate({
+    top: "0"
+  },1000);
+  $(" #tiburcia").animate({
+    top: "0"
+  },1000);
+
+  $("#pollito").animate({
+    left: "0"
+  },3000);
+  $("#ceviche").animate({
+    left: "0"
+  },3000);
+  $("#filete").animate({
+    left: "0"
+  },2000);
+  $("#pan").animate({
+    left: "0"
+  },2000);
+  $("#churrasco").animate({
+    left: "0"
+  },1000);
+  $("#anticuchos").animate({
+    left: "0"
+  },1000);
+
+
+
 })
